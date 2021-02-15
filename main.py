952afc206ac4b94397391a865d1ae3fb3b90e6cd
jenudi <jony101@gmail.com>
2021-02-15 19:33:09 +0200

    Updated files

--- main.py
+++ main.py
@@ -287,29 +287,3 @@ if __name__ == '__main__':
 # tensorboard --logdir=runs
 # %%
 
-import os
-import torch
-import torch.distributed as dist
-from torch.multiprocessing import Process
-
-
-def run(rank, size):
-    pass
-
-def init_process(rank, size, fn, backend='gloo'):
-    os.environ['MASTER_ADDR'] = '127.0.0.1'
-    os.environ['MASTER_PORT'] = '29500'
-    dist.init_process_group(backend, rank=rank, world_size=size)
-    fn(rank, size)
-
-
-if __name__ == "__main__":
-    size = 2
-    processes = []
-    for rank in range(size):
-        p = Process(target=init_process, args=(rank, size, run))
-        p.start()
-        processes.append(p)
-
-    for p in processes:
-        p.join()
